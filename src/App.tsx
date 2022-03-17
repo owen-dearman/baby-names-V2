ae39928dc320019d044c8947fa063d3cd986a8f2
Owen Dearman <owendearman@gmail.com>
2022-03-17 17:10:59 +0000

    working search bar

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,31 @@ import { babyNameData } from "./utils/babyNameData"
 import {babyNameDataInterface} from "./utils/babyNameDataInterface"
 import {sortNameList} from "./utils/sortNameList"
 import {mapBabyNames} from "./utils/mapBabyNames"
+import {useState} from "react"
 
 
 function App(): JSX.Element {
-  //sorts array of baby name objects into alphabetical order and maps them into JSX.Elements with CSS styling based on sex
-  const mainNameListItems = sortNameList(babyNameData).map(mapBabyNames)
 
-  return <div className="babyNamesList">{mainNameListItems}</div>
+    const [searchTerm, setSearchTerm] = useState<string>("")
+
+    //sorts array of baby name objects into alphabetical order 
+    const sortedNames = sortNameList(babyNameData)
+    //filters the names that match the input search
+    const searchedNameList:babyNameDataInterface[] = sortedNames.filter((nameObject) => nameObject.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    //Maps sorted & filtered names into JSX.Elements with CSS styling based on sex
+    const mainNameListItems = searchedNameList.map(mapBabyNames)
+
+
+   return <>
+      <input 
+      placeholder="Search Here"
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+    />
+    <hr />
+    <div className="babyNamesList">{mainNameListItems}</div>
+
+  </>
 }
 
 export default App;
