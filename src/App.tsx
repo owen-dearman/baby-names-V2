ea0d5d64f0fed49788d17c120a4a49b6c38fc6ed
Owen Dearman <owendearman@gmail.com>
2022-03-17 15:00:28 +0000

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import { greet } from "./utils/greet";
+import {babyNameData} from "./babyNameData"
 
 function App(): JSX.Element {
-  return <h1>{greet("World")}</h1>;
+  return <h1>{babyNameData}</h1>;
 }
 
 export default App;
