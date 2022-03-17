d10e54623ec42ef18703afacd3fa3516b8f34592
Owen Dearman <owendearman@gmail.com>
2022-03-17 16:27:17 +0000

    organise files into utils folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { babyNameData } from "./babyNameData"
-import {babyNameDataInterface} from "./babyNameDataInterface"
+import { babyNameData } from "./utils/babyNameData"
+import {babyNameDataInterface} from "./utils/babyNameDataInterface"
 import {sortNameList} from "./utils/sortNameList"
 import {mapBabyNames} from "./utils/mapBabyNames"
 
 
 function App(): JSX.Element {
-
+  //sorts array of baby name objects into alphabetical order and maps them into JSX.Elements with CSS styling based on sex
   const mainNameListItems = sortNameList(babyNameData).map(mapBabyNames)
 
   return <div className="babyNamesList">{mainNameListItems}</div>
--- src/utils/sortNameList.ts
+++ src/utils/sortNameList.ts
@@ -1,7 +1,7 @@
 import { babyNameDataInterface } from "../babyNameDataInterface";
 
  export function sortNameList (arrayOfNameObjects: babyNameDataInterface[]): babyNameDataInterface[] {
-     const sortedArray = arrayOfNameObjects.sort((a, b) => a.name.localeCompare(b.name));
+    const sortedArray = arrayOfNameObjects.sort((a, b) => a.name.localeCompare(b.name));
     return sortedArray
 }
 
