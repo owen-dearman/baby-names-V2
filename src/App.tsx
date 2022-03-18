02d46bddc4a7b4904e86876e85a52e1313c69be8
Owen Dearman <owendearman@gmail.com>
2022-03-18 11:27:06 +0000

    lint, format and typecheck

--- src/App.tsx
+++ src/App.tsx
@@ -7,30 +7,46 @@ function App(): JSX.Element {
   const [searchTerm, setSearchTerm] = useState<string>("");
   const [favourites, setToFavourites] = useState<babyNameDataInterface[]>([]);
 
- 
-  const alphabeticalOrderNames: babyNameDataInterface[] = sortNameList(babyNameData)
-  const filteredNames: babyNameDataInterface[] = alphabeticalOrderNames.filter((nameObject) =>
+  const alphabeticalOrderNames: babyNameDataInterface[] =
+    sortNameList(babyNameData);
+  const filteredNames: babyNameDataInterface[] = alphabeticalOrderNames
+    .filter((nameObject) =>
       nameObject.name.toLowerCase().includes(searchTerm.toLowerCase())
-    ).filter((x) => !favourites.includes(x))
+    )
+    .filter((x) => !favourites.includes(x));
   const preparedNames: JSX.Element[] = filteredNames.map(mapNamesToMain);
-  const preparedFavourites: JSX.Element[] = favourites.map(mapNamesToFavourites)
+  const preparedFavourites: JSX.Element[] =
+    favourites.map(mapNamesToFavourites);
 
   function mapNamesToMain(props: babyNameDataInterface): JSX.Element {
     return (
-      <div key={props.id} className={props.sex} onClick={() => setToFavourites([...favourites, alphabeticalOrderNames.filter((x) => x.id === props.id)[0]])   }>
+      <div
+        key={props.id}
+        className={props.sex}
+        onClick={() =>
+          setToFavourites([
+            ...favourites,
+            alphabeticalOrderNames.filter((x) => x.id === props.id)[0],
+          ])
+        }
+      >
         {props.name}
       </div>
     );
   }
   function mapNamesToFavourites(props: babyNameDataInterface): JSX.Element {
     return (
-      <div key={props.id} className={props.sex} onClick={() => setToFavourites(favourites.filter((x) => x.id !== props.id))}>
+      <div
+        key={props.id}
+        className={props.sex}
+        onClick={() =>
+          setToFavourites(favourites.filter((x) => x.id !== props.id))
+        }
+      >
         {props.name}
       </div>
     );
   }
-    
-  
 
   return (
     <>
@@ -43,7 +59,9 @@ function App(): JSX.Element {
       <h2>Favourites List</h2>
       <div className="babyNamesList">{preparedFavourites}</div>
       <hr />
-      <p>Now Showing {preparedNames.length} of {babyNameData.length} Names</p>
+      <p>
+        Now Showing {preparedNames.length} of {babyNameData.length} Names
+      </p>
       <div className="babyNamesList">{preparedNames}</div>
     </>
   );
