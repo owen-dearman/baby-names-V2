2e706b5de1c36f0d7063a723f47e58be8015af8c
Owen Dearman <owendearman@gmail.com>
2022-03-18 11:26:30 +0000

    working favourites bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,36 @@
 import { babyNameData } from "./utils/babyNameData";
-//import { babyNameDataInterface } from "./utils/babyNameDataInterface";
+import { babyNameDataInterface } from "./utils/babyNameDataInterface";
 import { sortNameList } from "./utils/sortNameList";
-import { mapBabyNames } from "./utils/mapBabyNames";
 import { useState } from "react";
 
 function App(): JSX.Element {
   const [searchTerm, setSearchTerm] = useState<string>("");
+  const [favourites, setToFavourites] = useState<babyNameDataInterface[]>([]);
 
-  //Sorts name data in alphabetical order, filters sorted names to matching search input and maps the filtered names into JSX.Elements
-  const sortedNames = sortNameList(babyNameData)
-    .filter((nameObject) =>
+ 
+  const alphabeticalOrderNames: babyNameDataInterface[] = sortNameList(babyNameData)
+  const filteredNames: babyNameDataInterface[] = alphabeticalOrderNames.filter((nameObject) =>
       nameObject.name.toLowerCase().includes(searchTerm.toLowerCase())
-    )
-    .map(mapBabyNames);
+    ).filter((x) => !favourites.includes(x))
+  const preparedNames: JSX.Element[] = filteredNames.map(mapNamesToMain);
+  const preparedFavourites: JSX.Element[] = favourites.map(mapNamesToFavourites)
+
+  function mapNamesToMain(props: babyNameDataInterface): JSX.Element {
+    return (
+      <div key={props.id} className={props.sex} onClick={() => setToFavourites([...favourites, alphabeticalOrderNames.filter((x) => x.id === props.id)[0]])   }>
+        {props.name}
+      </div>
+    );
+  }
+  function mapNamesToFavourites(props: babyNameDataInterface): JSX.Element {
+    return (
+      <div key={props.id} className={props.sex} onClick={() => setToFavourites(favourites.filter((x) => x.id !== props.id))}>
+        {props.name}
+      </div>
+    );
+  }
+    
+  
 
   return (
     <>
@@ -22,7 +40,11 @@ function App(): JSX.Element {
         onChange={(e) => setSearchTerm(e.target.value)}
       />
       <hr />
-      <div className="babyNamesList">{sortedNames}</div>
+      <h2>Favourites List</h2>
+      <div className="babyNamesList">{preparedFavourites}</div>
+      <hr />
+      <p>Now Showing {preparedNames.length} of {babyNameData.length} Names</p>
+      <div className="babyNamesList">{preparedNames}</div>
     </>
   );
 }
